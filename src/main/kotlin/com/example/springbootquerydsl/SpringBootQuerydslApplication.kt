9de1e78a011c450ad0b5bd069f2c21ad436bc997
김정근 <gogoogjg@tripstore.kr>
2023-11-24 11:47:03 +0900

    set up project

---
+++ src/main/kotlin/com/example/springbootquerydsl/SpringBootQuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootquerydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootQuerydslApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootQuerydslApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootquerydsl/SpringBootQuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootquerydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootQuerydslApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
